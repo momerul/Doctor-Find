3dc836565cbce2f74409b9ce4f21800e6d6119ff
Momarul Islam <36568905+momerul@users.noreply.github.com>
2019-07-17 21:29:55 +0600

    Update Patient Registration Form.aspx.cs

--- Patient Registration Form.aspx.cs
+++ Patient Registration Form.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 
+
 public partial class Patient_Registration_Form : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
